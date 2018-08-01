fddaec84a5656ee0805d6111ba97c14bb7be7cff
unknown <mrhdigital@yahoo.com>
2018-07-31 21:10:28 -0500

    change go to text

--- client/src/components/about.js
+++ client/src/components/about.js
@@ -6,7 +6,7 @@ const About = () => {
     <div>
         <h1> About </h1>
         <p> About text here </p>
-        <p> <Link to ="/products">Go to product page </Link> </p>
+        <p> <Link to ="/products">Go to MDF fretwork panel page </Link> </p>
         <p> <Link to ="/wavePanels">Go to 3d Wave Panel page </Link> </p>
     </div>
     )
