2d8df0ebddb203278b220318d4ad79ccf509e592
unknown <mrhdigital@yahoo.com>
2018-08-02 20:26:02 -0500

    added routes folder to generate catalogRoutes

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const mongoose = require('mongoose');
+const User = require('./models/User');
+const seedProducts = require('./seeds/products');
 
 const app = express();
 
