f0ac5e4c928f809cb912e82c189b91d2f951e68e
unknown <mrhdigital@yahoo.com>
2018-08-14 16:05:31 -0500

    fixed footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,7 +3,43 @@ import '../styles/Footer.css';
 
 const Footer = () => (
   <div className="footer">
-    <p>Decorative Panel Nook! &copy; 2018</p>
+    <div className="row">
+                <div className="col-md-4">
+                    <span className="copyright">Copyright &copy; 2018 Decorative Panel Nook!</span>
+                </div>
+                <div className="col-md-4">
+                    <ul className="list-inline social-buttons">
+                        <li className="list-inline-item">
+                            <h6 className="text-center text-light">Amal</h6>
+                            <a href="https://github.com/amalhassan007" target="blank">
+                                <i className="fab fa-github"></i>
+                            </a>
+                        </li>
+                        <li className="list-inline-item">
+                            <h6 className="text-center text-light">Mirza</h6>
+                            <a href="https://github.com/mrhdigital" target="blank">
+                                <i className="fab fa-github"></i>
+                            </a>
+                        </li>
+                        <li className="list-inline-item">
+                            <h6 className="text-center text-light">Pam</h6>
+                            <a href="https://github.com/pyang08" target="blank">
+                                <i classNameName="fab fa-github"></i>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <div className="col-md-4">
+                    <ul className="list-inline quicklinks">
+                        <li className="list-inline-item">
+                            <a href="#">Privacy Policy</a>
+                        </li>
+                        <li className="list-inline-item">
+                            <a href="#">Terms of Use</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
   </div>
 );
 
