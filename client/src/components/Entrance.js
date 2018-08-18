1949e71e10610bcfea9d9d9b5ec2f758c588566c
Pamela Yang <pamelapyang@hotmail.com>
2018-08-17 23:34:43 -0500

    changed css for login, footer and carousel

--- client/src/components/Entrance.js
+++ client/src/components/Entrance.js
@@ -1,45 +1,82 @@
 import React from 'react';
-import { Carousel } from "react-responsive-carousel";
+// import { Carousel } from "react-responsive-carousel";
+import { Carousel, CarouselItem } from 'react-bootstrap';
 
 
-export default () => (
-    <Carousel autoPlay>
-      <div>
-        <img className="Legend" src="./images/panel1.jpg" alt="one"/>
-        
-      </div>
-      <div>
-        <img src="./images/panel2.jpg"  alt="two"/>
-       
-      </div>
-      <div>
-        <img src="./images/panel3.jpg" alt="bla" />
-       
-      </div>
-      <div>
-        <img src="./images/panel4.jpg"  alt="bla"/>
-        
-      </div>
-      <div>
-        <img src="./images/panel5.jpg" alt="bla"/>
-        
-      </div>
-      <div>
-        <img src="./images/panel6.jpg" alt="bla" />
-       
-      </div>
-      <div>
-        <img src="./images/panel7.jpg" alt="bla" />
-      
-      </div>
-      <div>
-        <img src="./images/panel8.jpg" alt="bla"/>
-        
-      </div>
-      <div>
-        <img src="./images/panel9.jpg"alt="bla"/>
-        
-      </div>
-     
-      </Carousel>
-)
+const Entrance = () => (
+
+  <Carousel>
+    <Carousel.Item>
+      <img width={960} height={400} alt="960x400" src="./images/panel1.jpg" />
+    </Carousel.Item>
+    <Carousel.Item>
+      <img width={960} height={400} alt="960x400" src="./images/panel2.jpg" />
+    </Carousel.Item>
+    <Carousel.Item>
+      <img width={960} height={400} alt="960x400" src="./images/panel3.jpg" />
+    </Carousel.Item>
+    <Carousel.Item>
+      <img width={960} height={400} alt="960x400" src="./images/panel4.jpg" />
+    </Carousel.Item>
+    <Carousel.Item>
+      <img width={960} height={400} alt="960x400" src="./images/panel5.jpg" />
+    </Carousel.Item>
+    <Carousel.Item>
+      <img width={960} height={400} alt="960x400" src="./images/panel6.jpg" />
+    </Carousel.Item>
+    <Carousel.Item>
+      <img width={960} height={400} alt="960x400" src="./images/panel7.jpg" />
+    </Carousel.Item>
+    <Carousel.Item>
+      <img width={960} height={400} alt="960x400" src="./images/panel8.jpg" />
+    </Carousel.Item>
+    <Carousel.Item>
+      <img width={960} height={400} alt="960x400" src="./images/panel9.jpg" />
+    </Carousel.Item>
+  </Carousel>
+
+);
+
+export default Entrance;
+
+
+//     <Carousel autoPlay>
+//       <div>
+//         <img className="Legend" src="./images/panel1.jpg" alt="one"/>
+
+//       </div>
+//       <div>
+//         <img src="./images/panel2.jpg"  alt="two"/>
+
+//       </div>
+//       <div>
+//         <img src="./images/panel3.jpg" alt="bla" />
+
+//       </div>
+//       <div>
+//         <img src="./images/panel4.jpg"  alt="bla"/>
+
+//       </div>
+//       <div>
+//         <img src="./images/panel5.jpg" alt="bla"/>
+
+//       </div>
+//       <div>
+//         <img src="./images/panel6.jpg" alt="bla" />
+
+//       </div>
+//       <div>
+//         <img src="./images/panel7.jpg" alt="bla" />
+
+//       </div>
+//       <div>
+//         <img src="./images/panel8.jpg" alt="bla"/>
+
+//       </div>
+//       <div>
+//         <img src="./images/panel9.jpg"alt="bla"/>
+
+//       </div>
+
+//       </Carousel>
+// )
--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,15 +1,59 @@
 import React from 'react';
+import { Row, Col } from 'react-bootstrap';
 
 import '../styles/Footer.css';
 
 const Footer = () => (
   <div className="footer">
-    <p>Decorative Panel Nook! &copy; 2018 
-    <h5> Amal <a href="https://github.com/amalhassan007" target = "blank"><img src = "/img/github-circle.png"></img></a>
-     Pam <a href="https://github.com/pyang08" target = "blank"> <img src = "/img/github-circle.png"></img></a>
-    Mirza <a href="https://github.com/mrhdigital" target = "blank"><img src = "/img/github-circle.png"></img></a>
-    </h5>
-</p>
+    <p>Decorative Panel Nook! &copy; 2018 </p>
+
+    <Row className="show-grid">
+      <Col xs={6} md={4}>
+      </Col>
+      <Col xs={6} md={4}>
+
+        <Col xs={6} md={4}>
+          <a href="https://github.com/amalhassan007" target="blank">
+            Amal
+      </a>
+        </Col>
+        <Col xs={6} md={4}>
+          <a href="https://github.com/mrhdigital" target="blank">
+            Mirza
+    </a>
+        </Col>
+        <Col xsHidden md={4}>
+          <a href="https://github.com/pyang08" target="blank">
+            Pamela
+    </a>
+        </Col>
+      </Col>
+
+      <Col xsHidden md={4}>
+
+      </Col>
+    </Row>
+
+    <Row className="show-grid">
+      <Col xs={6} md={4}>
+      </Col>
+      <Col xs={6} md={4}>
+
+        <Col xs={6} md={4}>
+          <a href="https://github.com/amalhassan007" target="blank">
+            <img src="/img/github-circle.png" /></a>
+        </Col>
+        <Col xs={6} md={4}>
+          <a href="https://github.com/mrhdigital" target="blank"><img src="/img/github-circle.png"></img></a>
+        </Col>
+        <Col xsHidden md={4}>
+          <a href="https://github.com/pyang08" target="blank"><img src="/img/github-circle.png"></img></a>
+        </Col>
+      </Col>
+
+      <Col xsHidden md={4}>
+      </Col>
+    </Row>
   </div>
 );
 //title = {<img src = '/img/logo2.png' alt="Logo"  style={{height: 64}}/>}
