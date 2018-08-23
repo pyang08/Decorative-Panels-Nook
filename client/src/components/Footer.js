b61148150fea3e2a3a8cce431504b672146e3cf0
Pamela Yang <pamelapyang@hotmail.com>
2018-08-22 20:55:26 -0500

    centered footer and adjusted header - better for mobile

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -7,48 +7,52 @@ const Footer = () => (
   <div className="footer">
     <p>Decorative Panel Nook! &copy; 2018 </p>
 
-    <Row className="show-grid">
-      <Col xs={6} md={4}>
-      </Col>
-      <Col xs={6} md={4}>
 
-        <Col xs={6} md={4}>
+    <Row className="show-grid">
+      <Col xsHidden md={4}>
+      </Col>
+
+      <Col xs={12} sm={12} md={4}>
+
+        <Col xs={4} sm={4} md={4}>
           <a href="https://github.com/amalhassan007" target="blank">
             Amal
-      </a>
+          </a>
         </Col>
-        <Col xs={6} md={4}>
+        <Col xs={4} sm={4} md={4}>
           <a href="https://github.com/mrhdigital" target="blank">
             Mirza
-    </a>
+          </a>
         </Col>
-        <Col xsHidden md={4}>
+        <Col xs={4} sm={4} md={4}>
           <a href="https://github.com/pyang08" target="blank">
             Pamela
-    </a>
+          </a>
         </Col>
+
       </Col>
 
       <Col xsHidden md={4}>
-
       </Col>
+      
     </Row>
 
     <Row className="show-grid">
-      <Col xs={6} md={4}>
+      <Col xsHidden md={4}>
       </Col>
-      <Col xs={6} md={4}>
+      <Col xs={12} sm={12} md={4}>
 
-        <Col xs={6} md={4}>
+        <Col xs={4} sm={4} md={4}>
           <a href="https://github.com/amalhassan007" target="blank">
             <img src="/img/github-circle.png" /></a>
         </Col>
-        <Col xs={6} md={4}>
+        <Col xs={4} sm={4} md={4}>
           <a href="https://github.com/mrhdigital" target="blank"><img src="/img/github-circle.png"></img></a>
         </Col>
-        <Col xsHidden md={4}>
+        <Col xs={4} sm={4} md={4}>
           <a href="https://github.com/pyang08" target="blank"><img src="/img/github-circle.png"></img></a>
         </Col>
+
       </Col>
 
       <Col xsHidden md={4}>
@@ -56,6 +60,7 @@ const Footer = () => (
     </Row>
   </div>
 );
+
 //title = {<img src = '/img/logo2.png' alt="Logo"  style={{height: 64}}/>}
 
 // const Footer = () => (
